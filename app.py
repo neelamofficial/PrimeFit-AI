a5cc11bf6a6425ef966d28cbb0e017f9846a0664
neelamofficial <neelamofficial97@gmail.com>
2026-09-13 17:27:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if submit:
     with st.spinner("⚡ PrimeFit AI is crafting your customized plan..."):
         try:
             response = groq_client.chat.completions.create(
-                model="llama-3.3-70b-versatile",
+                model="openai/gpt-oss-120b",
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0.4,
                 max_tokens=2000
